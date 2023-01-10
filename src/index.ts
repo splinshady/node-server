5d95a9adbb17e40034c9f82c2500d42bc6861274
splinshady <splin.shady@gmail.com>
2023-01-10 16:40:22 +0300

    deploy: add vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-  let hello = 'He!';
+  let hello = 'Hi node!';
   res.send(hello)
 })
 
