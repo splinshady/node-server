5fa99a038062c3e68be0b191c883b0fd8fcd33bd
splinshady <splin.shady@gmail.com>
2023-02-11 18:22:35 +0300

    feat: add created time

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,13 @@
 import {gql} from "apollo-server-express";
 
 export const typeDefs = gql`
+  scalar DateTime
   type Note {
     id: ID
     content: String
     author: String
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
   type Query {
     notes: [Note]
